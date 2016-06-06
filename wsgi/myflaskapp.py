33e219fb0ae273270df5d1585a547c6802331f71
Luca Bonfante <bonfanteluca98@gmail.com>
2016-06-06 18:58:24 +0200

    added a database

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -4,22 +4,22 @@ from flask import Flask,request, send_from_directory
 from flask import render_template
 from flask import jsonify
 from json import dumps
+import pymongo
+import os
 #from flask.ext.cors import CORS
 
 
 app = Flask(__name__)
 #CORS(app)
-global registroAlunni
-registroAlunni = {0:{"numeroReg":0,"nome":"ignoto","cognome":"ignoto","annoNascita":"1900"}, 1:{"numeroReg":1,"nome":"ignoto1","cognome":"ignoto1","annoNascita":"1901"}}
 positions = {}
+conn = pymongo.Connection(os.environ['OPENSHIFT_NOSQL_DB_URL'])
+db = conn.database
+positions=db['positions']
+
 
 @app.route("/")
 def hello():
-    return render_template( 'index.html')
-
-@app.route("/test")
-def test():
-    return render_template( 'indexTest.html')
+    return render_template('index.html')
 
 @app.route("/js/<nomeFileJs>")
 def jsLoad(nomeFileJs):
@@ -28,51 +28,6 @@ def jsLoad(nomeFileJs):
 @app.route("/css/<nomeFileCss>")
 def cssLoad(nomeFileCss):
     return send_from_directory('css', nomeFileCss)
-   
-@app.route("/insertAlunno/")  # metodo GET per chiamare dalla barra del browser
-def inserisciAlunno ():
-    # spedizione in formato html
-    numeroReg =  request.args.get('numeroReg')
-    nome =       request.args.get('nome')
-    cognome =    request.args.get('cognome')
-    annoNascita =request.args.get('annoNascita') 
-    dizAlunno = { "numeroReg": numeroReg, "nome": nome,
-                  "cognome" : cognome , "annoNascita":annoNascita}
-    registroAlunni[int(numeroReg)]= dizAlunno
-    return "OK"   #restituisce status = 200  OK , ma nessuna stringa
-    
-    
-@app.route("/alunnoByNumeroReg/", methods=["POST"]) # metodo POST
-def alunnoByNumeroReg():
-    # spedizione in formato html
-    
-    numeroReg =  request.json['numeroReg']
-    
-    dizAlunno = registroAlunni[int(numeroReg)]
-    # in casi piu' complessi usare render_templates e quindi jsonify
-    stringJson = jsonify( ** dizAlunno)
-    return stringJson   #aggiunge content-type => json
-
-@app.route("/insertAlunnoPOST/", methods = ["POST"])
-def inserisciAlunnoPOST():
-    
-    numeroReg =     request.json['numeroReg']
-    nome =          request.json['nome']
-    cognome =       request.json['cognome']
-    annoNascita =   request.json['annoNascita']
-    
-    dizAlunno = {"numeroReg" : numeroReg, "nome" : nome, 
-                "cognome" : cognome, "annoNascita"  : annoNascita}
-                
-    registroAlunni[int(numeroReg)]= dizAlunno
-    print dizAlunno
-    print registroAlunni[int(numeroReg)]
-    return jsonify("")
-
-@app.route("/leggiDizionario/", methods = ["POST"])
-def leggiDizionario():
-    stringa = str(registroAlunni)
-    return stringa
 
 @app.route("/insertPosition/", methods = ["POST"])
 def insertPosition():
@@ -81,23 +36,50 @@ def insertPosition():
     longitude = request.json['longitude']
     date = request.json['date']
     
-    coordinate = {"latitude" : latitude, "longitude" : longitude}
+    coordinate = {"latitude" : latitude, "longitude" : longitude, "date":date}
+    positions[date]=coordinate
     
-    positions[date] = coordinate
-    print positions
-    return jsonify("")
+    positions.insert(coordinate)
+    lista = []
+    cursor = positions.find()
+    for document in cursor:
+        lista.append(document)
+    return jsonify(document)
 
 @app.route("/readPositions/", methods = ["POST"])
 def readPositions():
-    stringa = str(positions)
-    return stringa
+    risultato="<p>"
+    for coordinate in positions:
+        latitudine = positions[coordinate]["latitude"]
+        longitudine = positions[coordinate]["longitude"]
+        data = positions[coordinate]["date"]
+        risultato += '  <div class=""> \
+                            <table style="width:90%"> \
+                                <tr> \
+                                    <td><b>Latitudine:&nbsp;&nbsp;&nbsp;</b></td> \
+                                    <td><b>Longitudine:&nbsp;&nbsp;&nbsp;</b></td> \
+                                    <td><b>Data:</b></td> \
+                                    <td><a href="#" class="x" onclick="APP.eliminaPosition();">x</a></td> \
+                                </tr> \
+                                <tr> \
+                                    <td>' + latitudine + '</td> \
+                                    <td>' + longitudine + '</td> \
+                                    <td>' + data + '</td> \
+                                </tr> \
+                            </table> \
+                        </div> '
+    risultato += "</p>"
+    return risultato
 
 @app.route("/cleanPositions/", methods = ["POST"])
 def cleanPositions():
-    positions = {}
+#    date = request.json[pos]
+#    positions[date] = {}
     return ""
     
 if __name__ == "__main__":
     #app.debug=True
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
     #65013
+    
+    #\'' + coordinate + '\'
--- wsgi/static/js/app.js
+++ wsgi/static/js/app.js
@@ -7,80 +7,11 @@
 //author : lucabonfante
 
 var APP = {
-    init_leggiCampoText1_click: function () {
-        $('#leggiCampoText1').on('click', function ()
-        {
-            numeroReg = $("#campoText1").val();
-            $.ajax(
-                    {method: "POST",
-                        url: "/alunnoByNumeroReg/",
-                        contentType: 'application/json',
-                        crossDomain: true,
-                        type: "json",
-                        data: JSON.stringify({"numeroReg": numeroReg}),
-                        dataType: "json",
-                        success: function (data) {
-                            contenuto = data.numeroReg + " " +
-                                        data.nome + " " +
-                                        data.cognome + " " +
-                                        data.annoNascita;
-                            $("#campoText1-div").html(contenuto);
-                        },
-                        error: function (data) {
-                            $("#campoText1-div").html("errore");
-                        }
-                    });
-        });
-    },
-    init_leggiCampoText2_click: function () {
-        $('#leggiCampoTextRegistrazione').on('click', function ()
-        {
-            numeroReg = $("#campoTextNumReg").val();
-            nome = $("#campoTextNome").val();
-            cognome = $("#campoTextCognome").val();
-            annoNascita = $("#campoTextAnnoNascita").val();
-            $.ajax(
-                    {method: "POST",
-                        url: "/insertAlunnoPOST/",
-                        contentType: 'application/json',
-                        crossDomain: true,
-                        type: "json",
-                        data: JSON.stringify({"numeroReg": numeroReg, "nome": nome, "cognome": cognome, "annoNascita": annoNascita}),
-                        dataType: "json",
-                        success: function (data) {
-                            alert("ok");
-                        },
-                        error: function () {
-                            alert("errore");
-                        }
-                    });
-        });
-    },
-    init_printDizionario_click: function () {
-        $('#leggiDizionario').on('click', function () {
-            $.ajax(
-                    {
-                        method: "POST",
-                        url: "/leggiDizionario/",
-                        contentType: "application/json",
-                        crossDomain: true,
-                        type: "json",
-                        dataType: "text",
-                        success: function (data) {
-                            $("#campoText3-div").html(data);
-                        },
-                        error: function () {
-                            $("#campoText3-div").html("errore");
-                        }
-                    });
-        });
-
-    },
-    init_coordinate_click: function () {
+    aggiungiCoordinate: function () {
         $('#inviaCoordinate').on('click', function () {
-            longitude = $('#longitude').val();
-            latitude = $('#latitude').val();
-            date = $('#date').val();
+            var longitude = $('#longitude').val();
+            var latitude = $('#latitude').val();
+            var date = new Date();
             $.ajax(
                     {
                         method: "POST",
@@ -91,59 +22,79 @@ var APP = {
                         data: JSON.stringify({"longitude": longitude, "latitude": latitude, "date": date}),
                         dataType: "json",
                         success: function (data) {
-                            alert('ok');
+                            APP.leggiPosizioni();
+                            APP.maps(latitude, longitude);
+                            console.info(data);
                         },
                         error: function () {
                             alert('errore');
                         }
                     });
         });
+
     },
-    init_readPositions_click: function () {
-        $("#buttonReadPositions").on('click', function () {
-            $.ajax(
-                    {
-                        method: "POST",
-                        url: "/readPositions/",
-                        contentType: "application/json",
-                        crossDomain: true,
-                        type: "json",
-                        dataType: "text",
-                        success: function (data) {
-                            $("#campoTextPositions").html(data);
-                        },
-                        error: function () {
-                            $("#campoTextPositions").html("errore");
-                        }
-                    });
-        });
+    leggiPosizioni: function () {
+        $.ajax(
+                {
+                    method: "POST",
+                    url: "/readPositions/",
+                    contentType: "application/json",
+                    crossDomain: true,
+                    type: "json",
+                    dataType: "HTML",
+                    success: function (result) {
+                        $("#campoTextPositions").html(result);//html da stampare
+                    },
+                    error: function () {
+                        $("#campoTextPositions").html("errore");
+                    }
+                });
     },
-    init_cleanPositions_click: function(){
-        $("#buttonCleanPositions").on('click', function(){
-            $.ajax(
-                    {
-                        method: "POST",
-                        url: "/cleanPositions/",
-                        contentType: "application/json",
-                        crossDomain: true,
-                        type: "json",
-                        dataType: "text",
-                        success: function (data) {
-                            alert("cancellato tutto!");
-                        },
-                        error: function () {
-                            alert("errore");
-                        }
+    eliminaPosition: function (codice) {
+        $.ajax(
+                {
+                    method: "POST",
+                    url: "/cleanPositions/",
+                    contentType: "application/json",
+                    crossDomain: true,
+                    type: "json",
+                    data: JSON.stringify({"pos": codice}),
+                    dataType: "text",
+                    success: function (data) {
+                    },
+                    error: function () {
+                    }
+                });
+
+    },
+    maps: function (latitude, longitude) {
+        var myCenter = new google.maps.LatLng(43, 67);
+
+        function initialize()
+        {
+            var mapProp = {
+                center: myCenter,
+                zoom: 5,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+            var marker = new google.maps.Marker({
+                position: myCenter,
             });
-        });
+
+            marker.setMap(map);
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
     }
+
 };
 $(document).ready(function () {
 
-    APP.init_leggiCampoText1_click();
-    APP.init_leggiCampoText2_click();
-    APP.init_printDizionario_click();
-    APP.init_coordinate_click();
-    APP.init_readPositions_click();
-    APP.init_cleanPositions_click();
+    APP.aggiungiCoordinate();
+    APP.leggiPosizioni();
+    APP.eliminaPosition();
+    APP.maps();
 });
